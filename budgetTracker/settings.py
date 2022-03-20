0f1a61f5d6977d8986bdd88838703199f8c603b3
samrecile <recilesam@gmail.com>
2022-03-20 11:41:16 -1000

    staticfile configuration

--- budgetTracker/settings.py
+++ budgetTracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) 
 
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
--- budgetTracker/urls.py
+++ budgetTracker/urls.py
@@ -7,4 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
