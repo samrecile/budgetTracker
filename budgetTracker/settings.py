a285ebd9cf8c514f4bcfdf6861378a289e2deb59
samrecile <recilesam@gmail.com>
2022-03-19 23:38:01 -1000

    static directory

--- budgetTracker/settings.py
+++ budgetTracker/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) 
+
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
